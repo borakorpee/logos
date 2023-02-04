7f77c403a7f8f507f5a88e25fe7efa9267edd6a7
Bora Korpe <borakorpee@gmail.com>
2023-02-04 21:54:48 +0300

    tab screens

--- lib/screens/home/favorites_page.dart
+++ lib/screens/home/favorites_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
@@ -6,7 +7,31 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("FavoritesPage")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 75).r,
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Tercih Edilen Psikiyatri Doktorları",
+                style: TextStyle(color: Colors.black, fontSize: 15.sp),
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Herhangi bir psikolog kayıt etmediniz. Kendinize uygun bir psikolog seçip kaydederek psikoloğunuza dilediğiniz zaman kolay bir şekilde ulaşabilirsiniz.",
+                    style: TextStyle(color: Colors.black, fontSize: 15.sp),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/screens/home/history_page.dart
+++ lib/screens/home/history_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HistoryPage extends StatelessWidget {
   const HistoryPage({super.key});
@@ -6,7 +7,32 @@ class HistoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("HistoryPage")),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 75).r,
+        child: SizedBox(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Geçmiş Randevular",
+                style: TextStyle(color: Colors.black, fontSize: 15.sp),
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Herhangi bir randevu geçmişiniz bulunmamakta. Kendinize uygun bir psikolog seçerek randevu alabilirsiniz.",
+                    style: TextStyle(color: Colors.black, fontSize: 15.sp),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
